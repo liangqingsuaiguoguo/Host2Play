5c8710dff1d7cba22350de588b14f4231b4a5dae
liangqingsuaiguoguo <tan_haoran@icloud.com>
2026-06-04 13:57:58 +0800

    new www

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=daf43013-8cf3-403b-b310-da0cccca8494",
     # 添加更多链接
 ]
 
